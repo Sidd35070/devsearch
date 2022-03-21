72d68656295441f81f2c0b68d21690a116267e4e
Siddharth Rai <sidd35070@gmail.com>
2022-03-21 22:56:51 +0530

    images

--- projects/models.py
+++ projects/models.py
@@ -6,6 +6,7 @@ import uuid
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank = True)
+    featuredImage = models.ImageField(null=True, blank=True, default='default.jpg')
     demo_link = models.CharField(max_length=2000, null=True, blank = True)
     source_link = models.CharField(max_length=2000, null=True, blank = True)
     tags = models.ManyToManyField('Tag', blank=True)
@@ -13,7 +14,6 @@ class Project(models.Model):
     vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-
 
     def __str__(self):
         return self.title
@@ -36,6 +36,7 @@ class Review(models.Model):
 
 
 class Tag(models.Model):
+
     name = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
